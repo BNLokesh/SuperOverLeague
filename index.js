40f29fa48a4afa61d25da6adc44411cb66987c19
BNLokesh <lokeshbn2000@gmail.com>
2024-03-11 07:25:30 +0000

    initialcommit

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div class='bg-container'>
+    <h1 class='main-heading'>Super Over League</h1>
+    <div class='image-container'>
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
+        class='img-sec'
+      />
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
+        class='img-sec'
+      />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
